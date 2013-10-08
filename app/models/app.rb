fde175348a0ca59e083000194b720d7ff86d8096
Aaron Wishnick <Aaron_B.Wishnick@tufts.edu>
2013-10-08 12:45:20 -0400

    added data validation

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,4 @@
 class App < ActiveRecord::Base
 	has_many :messages
+	validates_presence_of :app_title, :description
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
 	belongs_to :app
+	validates :username, :format => {:with => /([A-Za-z0-9\-\_]+)/}
+	validates_presence_of :app_id, :content, :username
+	validates_length_of :content, :in => 1..160
+	validates_length_of :username, :in => 3..60
 end
