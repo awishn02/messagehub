e36397ab9730ffcc495af4bc8a9a4eed5beef88f
Aaron Wishnick <aaronwishnick@gmail.com>
2013-11-06 16:02:47 -0500

    added logging

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,7 +1,10 @@
 class RegisterController < ApplicationController
 
   def index
-    Urbanairship.register_device(params[:device_token])
-    Device.new(params[:device_token])
+    Urbanairship.register_device(params[:device][:device_token])
+    Device.new(params[:device])
+    puts params
+    puts params[:device]
+    puts params[:device][:device_token]
   end
 end
