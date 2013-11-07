ab841a9997a25035a58c25f55796a609379e9123
Aaron Wishnick <aaronwishnick@gmail.com>
2013-11-06 20:48:22 -0500

    added urban airship config

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -1,10 +1,13 @@
 class RegisterController < ApplicationController
-
+  skip_before_filter :verify_authenticity_token
   def index
-    Urbanairship.register_device(params[:device][:device_token])
-    Device.new(params[:device])
     puts params
     puts params[:device]
     puts params[:device][:device_token]
+    Urbanairship.register_device(params[:device][:device_token])
+    Device.new(params[:device])
+    respond_to do |format|
+      format.json {render :text => params[:device]}
+    end
   end
 end
