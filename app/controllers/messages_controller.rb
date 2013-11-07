f7eacb866b002b0eefd5271fe9e1899c1beafc33
Aaron Wishnick <aaronwishnick@gmail.com>
2013-11-06 22:42:15 -0500

    removed dependency on device model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -12,14 +12,14 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(params.require(:message).permit(:username, :content, :app_id))
     #devices = Device.where.not(device_token: params.require(:message).permit(:device_token))
-    devices = Device.all
-    device_tokens = []
-    devices.each do |device|
-      device_tokens.push(device.device_token)
-    end
+    # devices = Device.all
+    # device_tokens = []
+    # devices.each do |device|
+    #   device_tokens.push(device.device_token)
+    # end
     notification = {
       :schedule_for => Time.now,
-      :device_tokens => device_tokens,
+      :device_tokens => ['C44CA5DC5B47038CEF7802010C514A3564396828BBB643CF861220EAD5961C9B'],
       :aps => {:alert => 'You have new messages!', :badge => 1}
     }
     Urbanairship.push notification
