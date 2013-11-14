afc188de738ca05fcbefb2d6fc9b11e5c39bdb14
Aaron Wishnick <aaronwishnick@gmail.com>
2013-11-14 17:35:46 -0500

    only send push if successful send

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,19 +11,18 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(params.require(:message).permit(:username, :content, :app_id))
-    devices = Device.where.not(device_token: params[:message][:device_token])
-    #devices = Device.all
-    device_tokens = []
-    devices.each do |device|
-      device_tokens.push(device.device_token)
-    end
-    notification = {
-      :device_tokens => device_tokens,
-      :aps => {:alert => 'You have new messages!', :badge => 1}
-    }
-    Urbanairship.push(notification)
     respond_to do |format|
       if @message.save
+        devices = Device.where.not(device_token: params[:message][:device_token])
+        device_tokens = []
+        devices.each do |device|
+          device_tokens.push(device.device_token)
+        end
+        notification = {
+          :device_tokens => device_tokens,
+          :aps => {:alert => 'You have new messages!', :badge => 1}
+        }
+        Urbanairship.push(notification)
         format.json {render json: @message}
       else
         format.json {render json: @message.errors, :status => 500}
